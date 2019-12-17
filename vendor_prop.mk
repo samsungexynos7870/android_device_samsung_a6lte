49710f5519450e597f1218376ec4c1a5611a7c96
Astrako <apamarin@gmail.com>
2019-12-17 10:38:50 +0100

    Initial a6lte device tree

---
+++ vendor_prop.mk
@@ -0,0 +1,11 @@
+# DPI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320
+
+# DUAL SIM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.multisim.simslotcount=2
+
+# Fix for echo in calls
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio_hal.disable_two_mic=false
