e70ada39b3f08570a5c9f800c678a285b746f302
Ksawlii <153387544+Ksawlii@users.noreply.github.com>
2024-07-04 00:46:09 +0200

    Update and rename havoc_a6lte.mk to lineage_a6lte.mk

--- lineage_a6lte.mk
+++ lineage_a6lte.mk
@@ -14,30 +14,30 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from a3y17lte device
+$(call inherit-product, device/samsung/j6lte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from a6lte device
-$(call inherit-product, device/samsung/a6lte/device.mk)
-
-# Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a6lte
-PRODUCT_NAME := havoc_a6lte
+PRODUCT_NAME := lineage_a6lte
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := Galaxy A6
+PRODUCT_MODEL := SM-A600F
 PRODUCT_MANUFACTURER := samsung
 
-WITH_GAPPS := false
-TARGET_SUPPORTS_BLUR := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-TARGET_VENDOR := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
+
+# Build info
+BUILD_FINGERPRINT :="samsung/beyond2ltexx/beyond2:10/QP1A.190711.020/G975FXXU3BSKO:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="a6lte" \
+    PRODUCT_NAME="lineage_a6lte"
